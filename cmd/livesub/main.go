0f049758fdf969a287d97fc254872af8e9cc4716
MatchaCake <29390506+MatchaCake@users.noreply.github.com>
2026-02-18 16:23:51 +0800

    fix: inline openBrowser into main.go (fix CI build)

--- cmd/livesub/main.go
+++ cmd/livesub/main.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"os/exec"
 	"os/signal"
+	"runtime"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -597,4 +599,17 @@ func isTargetLang(detected, target string) bool {
 		return true
 	}
 	return false
+}
+
+func openBrowser(url string) {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	default:
+		cmd = exec.Command("xdg-open", url)
+	}
+	_ = cmd.Start()
 }
